44a0b3747944aa7d3d828a4e8b46f63adaf8b098
Noorul-Ayn <h.bello@alustudent.com>
2025-05-21 18:24:14 +0200

    commit for .

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,24 +9,24 @@ from datetime import datetime
 from models.engine.file_storage import FileStorage
 from models import storage
 
-class TestFileStorage_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the FileStorage class."""
+# class TestFileStorage_instantiation(unittest.TestCase):
+#     """Unittests for testing instantiation of the FileStorage class."""
 
-    def test_FileStorage_instantiation_no_args(self):
-        self.assertEqual(type(FileStorage()), FileStorage)
+#     def test_FileStorage_instantiation_no_args(self):
+#         self.assertEqual(type(FileStorage()), FileStorage)
 
-    def test_FileStorage_instantiation_with_arg(self):
-        with self.assertRaises(TypeError):
-            FileStorage(None)
+#     def test_FileStorage_instantiation_with_arg(self):
+#         with self.assertRaises(TypeError):
+#             FileStorage(None)
 
-    def test_FileStorage_file_path_is_private_str(self):
-        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+#     def test_FileStorage_file_path_is_private_str(self):
+#         self.assertEqual(str, type(FileStorage._FileStorage__file_path))
 
-    def testFileStorage_objects_is_private_dict(self):
-        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+#     def testFileStorage_objects_is_private_dict(self):
+#         self.assertEqual(dict, type(FileStorage._FileStorage__objects))
 
-    def test_storage_initializes(self):
-        self.assertEqual(type(storage), FileStorage)
+#     def test_storage_initializes(self):
+#         self.assertEqual(type(storage), FileStorage)
 
 
 class TestFileStorage_methods(unittest.TestCase):
@@ -98,10 +98,6 @@ class TestFileStorage_methods(unittest.TestCase):
     def test_save_with_arg(self):
         with self.assertRaises(TypeError):
             storage.save(None)
-
-    # def test_reload_with_arg(self):
-    #     with self.assertRaises(TypeError):
-    #         storage.reload(None)
 
 
 if __name__ == "__main__":
